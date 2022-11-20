76fa04f5111d0799c6e14fec69ccb9259abc7b39
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-11-20 20:40:44 +0100

    card article responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './assets/css/style.css'
+import { CardArticle } from './components/CardArticle';
+import {CardDesktop} from './components/CardDesktop'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardArticle></CardArticle>
+      <CardDesktop></CardDesktop>
     </div>
   );
 }
